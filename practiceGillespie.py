98c251849194b0fc242c2f7bf21a89edc60150c1
Greniza <cooldudegreniza@gmail.com>
2020-03-02 13:33:06 -0800

    added partner 1 first name

--- practiceGillespie.py
+++ practiceGillespie.py
@@ -1,13 +1,16 @@
+# Partner A: Peter Gillepsie
+# 
+
 def getNRandom(n):
-	'''takes in an integer and returns a list of n random integers between 1 and 10, inclusive'''
+    '''takes in an integer and returns a list of n random integers between 1 and 10, inclusive'''
     pass
 
 def multiplyRandom(numbers):
-	'''takes in a list of n numbers and returns the product of the numbers'''
+    '''takes in a list of n numbers and returns the product of the numbers'''
     pass
 
 def main():
-	print(multiplyRandom(getNRandom(10))
+    print(multiplyRandom(getNRandom(10))
 
-if __name__ == "__main__":
-	main()
+if __name__ == '__main__':
+    main()
